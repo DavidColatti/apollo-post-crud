96b12f14ba33ba7f8c2cfc5cf53987e6c016c28f
David Colatti <davidcolatti@gmail.com>
2022-01-11 21:13:21 -0500

    feat: edit and delete mutations

--- src/graphql/resolvers/post.js
+++ src/graphql/resolvers/post.js
@@ -4,11 +4,35 @@ export default {
       const posts = await Post.find();
       return posts;
     },
+    getPostById: async (_, { id }, { Post }) => {
+      const post = await Post.findById(id);
+      return post;
+    },
   },
   Mutation: {
     createNewPost: async (_, { newPost }, { Post }) => {
-      const post = await Post.create(newPost);
-      return post;
+      const createdPost = await Post.create(newPost);
+      return createdPost;
+    },
+    editPostById: async (_, { id, updatedPost }, { Post }) => {
+      const editedPost = await Post.findByIdAndUpdate(
+        id,
+        {
+          ...updatedPost,
+        },
+        { new: true }
+      );
+
+      return editedPost;
+    },
+    deletePostById: async (_, { id }, { Post }) => {
+      const deletedPost = await Post.findByIdAndDelete(id);
+
+      return {
+        success: true,
+        id: deletedPost.id,
+        message: "Your post is deleted.",
+      };
     },
   },
 };
--- src/graphql/typeDefs/post.js
+++ src/graphql/typeDefs/post.js
@@ -3,23 +3,33 @@ import { gql } from "apollo-server-express";
 export default gql`
   extend type Query {
     getAllPosts: [Post!]!
+    getPostById(id: ID!): Post!
   }
 
   extend type Mutation {
-    createNewPost(newPost: NewPost): Post!
+    createNewPost(newPost: PostInput!): Post!
+    deletePostById(id: ID!): PostNotification!
+    editPostById(updatedPost: PostInput!, id: ID!): Post!
   }
 
-  input NewPost {
+  input PostInput {
     title: String!
     content: String!
     featuredImage: String
   }
 
   type Post {
+    id: ID!
     title: String!
     content: String!
     featuredImage: String
     createdAt: String
     updatedAt: String
+  }
+
+  type PostNotification {
+    id: ID!
+    message: String!
+    success: Boolean!
   }
 `;
